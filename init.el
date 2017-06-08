54f1a9b39c7c7a558183de6727c596228f909dc9
jchkoch <jckoch@ualberta.ca>
2017-06-07 21:25:06 -0600

    initial commit of emacs config

---
+++ init.el
@@ -0,0 +1,45 @@
+;; init.el --- Emacs Configuration
+;; ------------------------------------------------
+
+;; Install Packages
+;; ------------------------------------------------
+
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Basic Customizations
+;; ------------------------------------------------
+
+(when window-system
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1))
+
+(setq inhibit-startup-message t)
+(setq initial-scratch-message "")
+
+;; Start the server
+;; ------------------------------------------------
+
+(server-start)
+
+;; Load the config
+;; ------------------------------------------------
+
+(org-babel-load-file (concat user-emacs-directory "config.org"))
+
+;; Set customized variables to seperate file
+;; ------------------------------------------------
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)

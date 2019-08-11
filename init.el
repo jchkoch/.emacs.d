97f59a6591f4422253e12a54fdd8abeaeeeb3439
jchkoch <jchkoch@gmail.com>
2019-08-11 01:16:36 -0600

    ispell update

--- init.el
+++ init.el
@@ -72,13 +72,13 @@
 ;; ------------------------------------------------
 (server-start)
 
-;; Load the config
-;; ------------------------------------------------
-(org-babel-load-file (concat user-emacs-directory "config.org"))
-
 ;; Load personal config
 ;; -----------------------------------------------
 (org-babel-load-file (concat user-emacs-directory "personal.org"))
+
+;; Load the config
+;; ------------------------------------------------
+(org-babel-load-file (concat user-emacs-directory "config.org"))
 
 ;; Set customized variables to seperate file
 ;; ------------------------------------------------

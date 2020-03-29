460132545c40dc831efde068aeab75c884aac647
jchkoch <jchkoch@gmail.com>
2020-03-29 11:02:55 -0600

    keep working on config reorganisation

--- init.el
+++ init.el
@@ -6,12 +6,12 @@
 ;; modify it under the terms of the GNU General Public License
 ;; as published by the Free Software Foundation; version 2
 ;; of the License.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; if not, see <https://www.gnu.org/licenses/>.
 
@@ -29,9 +29,21 @@
 (setq package-enable-at-startup nil)
 
 (setq package-archives
-      '(("melpa"        . "https://melpa.org/packages/")))
-  
+      '(("elpa"         . "https://elpa.gnu.org/packages/")
+        ("melpa"        . "https://melpa.org/packages/")))
+
 (package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (setq use-package-always-ensure nil)
+  (setq use-package-always-defer nil)
+  (setq use-package-always-demand nil)
+  (setq use-package-expand-minimally nil)
+  (setq use-package-enable-imenu-support t))
 
 (require 'org)
 (setq vc-follow-symlinks t)

e90dda8ed693d95c21c9a8a2c9c7de92a470c612
jchkoch <jchkoch@gmail.com>
2020-11-08 22:05:09 +0100

    update init.el/add early-init.el for Emacs >27

--- init.el
+++ init.el
@@ -26,25 +26,50 @@
 
 (require 'package)
 
-(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa"        . "https://melpa.org/packages/"))
 
-(setq package-archives
-      '(("elpa"         . "https://elpa.gnu.org/packages/")
-        ("melpa"        . "https://melpa.org/packages/")))
+;; (package-initialize)
 
-(package-initialize)
-
+;; Make sure `use-package' is available.
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; Configure `use-package' prior to loading it.
 (eval-when-compile
   (setq use-package-always-ensure nil)
   (setq use-package-always-defer nil)
   (setq use-package-always-demand nil)
   (setq use-package-expand-minimally nil)
-  (setq use-package-enable-imenu-support t))
+  (setq use-package-enable-imenu-support t)
+  (setq use-package-compute-statistics nil)
+  ;; The following is IMPORTANT. Write hooks using their real name
+  ;; instead of a shorter version: after_init ==> `after-init-hook'
+  ;;
+  ;; This is to empower help commands with their contextual awareness,
+  ;; such as `describe-symbol'.
+  (setq use-package-hook-name-suffix nil))
 
-(require 'org)
-(setq vc-follow-symlinks t)
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(eval-when-compile
+  (require 'use-package))
+
+(use-package vc
+  :config
+  (setq vc-follow-symlinks t)) ; Because my dotfiles are managed that way
+
+;; For my custom libraries
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+;; since Emacs 27.1
+(let* ((conf "~/.emacs.d/config")
+       (el (concat conf ".el"))
+       (org (concat conf ".org")))
+  (if (file-exists-p el)
+      (load-file el)
+    (use-package org)
+    (org-babel-load-file org)))
+
+;; prior to Emacs 27.1
+;; Load my Org configuration file
+;(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
